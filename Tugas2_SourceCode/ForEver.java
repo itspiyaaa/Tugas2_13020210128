38c9855050d4d924462db8e6f39f7a546cfd5a2d
itspiyaaa <125229201+itspiyaaa@users.noreply.github.com>
2023-03-26 11:08:14 +0800

    Tugas-2
    Penambahan Source code tugas 2

---
+++ Tugas2_SourceCode/ForEver.java
@@ -0,0 +1,19 @@
+/* Nama : Siti Lutfiah Agus
+NIM : 13020210128
+Waktu : 14/03/2023 09.39 PM */
+
+public class ForEver {
+
+	/**
+	 * @param args
+	*/ 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/* Program */
+		System.out.println("Program akan looping, akhiri dengan ^c");
+		while (true) 
+		{	System.out.print ("Print satu baris.....\n");
+
+		}
+	}
+}
---
+++ Tugas2_SourceCode/If3.java
@@ -0,0 +1,38 @@
+/* Nama : Siti Lutfiah Agus
+Nim : 13020210128
+Waktu : 18/3/2023 10.00 PM */
+
+import java.util.Scanner;
+
+/* contoh pemakaian IF tiga kasus */
+/* Membaca sebuah nilai */
+/* menuliskan 'Nilai a positif , nilai a', jika a >0 */
+/* 'Nilai Nol , nilai a', jika a = 0 */
+/* 'Nilai a negatif, nilai a', jika a <0 */
+
+public class If3 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub 
+		/* Kamus */
+		int a;
+		Scanner masukan=new Scanner (System.in);
+		/* Program */
+		System.out.print ("Contoh IF tiga kasus \n");
+		System.out.print ("Ketikkan suatu nilai integer :");
+		a=masukan.nextInt();
+		if (a > 0) {
+			System.out.println ("Nilai a positif "+ a);
+		}else if (a == 0) {
+			System.out.println ("Nilai Nol "+ a);
+		}else /* a > 0 */ {
+			System.out.println ("Nilai a negatif "+ a);
+		}
+
+	}
+
+}
+		
